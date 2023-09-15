b020d200f63112d97042e3ec0b126472a04954e6
PradeepaBharathi <bharathi83201@gmail.com>
2023-09-15 11:22:51 +0530

    first commit

---
+++ src/Context/context.js
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext } from "react";
+
+const NameContext = createContext(); //1
+
+const NameProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({fname:"", lname:"", email:"", id:""});
+  const [isLogged, setIsLogged] = useState(false) 
+  const serverURL = "https://url-backend-z3ta.onrender.com";
+
+  return (
+    
+    <NameContext.Provider 
+      value={{ currentUser, setCurrentUser, isLogged, setIsLogged, serverURL }}
+    >
+      {children}
+    </NameContext.Provider>
+  );
+};
+
+ const useGlobalContext = () => {
+    return useContext(NameContext);
+  };
+  
+  export { NameContext, NameProvider, useGlobalContext };
